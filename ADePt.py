bb84d755e2b569bba7ef0d62e0da29bbbcbd0863
Iksung Kang <47669194+iksungk@users.noreply.github.com>
2023-07-08 07:11:37 -0700

    Update ADePt.py

--- ADePt.py
+++ ADePt.py
@@ -438,38 +438,6 @@ class DecoderBranch(nn.Module):
         x = torch.sigmoid(x) * (self.sigmoid_max - self.sigmoid_min) + self.sigmoid_min
         
         return x
-    
-    
-class Discriminator(nn.Module):
-    def __init__(self, input_nc, ndf=32, n_layers=5, norm_layer=nn.BatchNorm3d):
-        super(Discriminator, self).__init__()
-        kw = 4
-        padw = 1
-        sequence = [nn.Conv3d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
-        nf_mult = 1
-        nf_mult_prev = 1
-        for n in range(1, n_layers):  # gradually increase the number of filters
-            nf_mult_prev = nf_mult
-            nf_mult = min(2 ** n, 8)
-            sequence += [
-                nn.Conv3d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=False),
-                norm_layer(ndf * nf_mult),
-                nn.LeakyReLU(0.2, True)
-            ]
-
-        nf_mult_prev = nf_mult
-        nf_mult = min(2 ** n_layers, 8)
-        sequence += [
-            nn.Conv3d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=False),
-            norm_layer(ndf * nf_mult),
-            nn.LeakyReLU(0.2, True)
-        ]
-
-        sequence += [nn.Conv3d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw), nn.Sigmoid()]  # output 1 channel prediction map
-        self.model = nn.Sequential(*sequence)
-
-    def forward(self, input):
-        return self.model(input)
 
     
 class noise_net(nn.Module):
